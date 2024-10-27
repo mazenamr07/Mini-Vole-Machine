2fcb84c490b4bfa55e95467cccedf7a172548698
Mazen Amr <160724290+mazenamr07@users.noreply.github.com>
2024-10-27 23:22:24 +0300

    updated function to prepare for negative numbers

--- CPU.cpp
+++ CPU.cpp
@@ -1,66 +1,86 @@
 #include "CPU.h"
 
 //    Hexadecimal
-double ALU::hexToDec(const std::string &hex) {
-    double result = 0;
+int ALU::hexToDec(const std::string &hex, bool isTwosComplement, bool isFloatNotation) {
+    if (!isTwosComplement && !isFloatNotation) {
+        int result = 0;
 
-    for (char i: hex) {
-        if (i >= '0' && i <= '9') {
-            result = result * 16 + (i - '0');
-        } else if (i >= 'A' && i <= 'F') {
-            result = result * 16 + (i - 'A' + 10);
-        } else if (i >= 'a' && i <= 'f') {
-            result = result * 16 + (i - 'a' + 10);
+        for (char i: hex) {
+            if (i >= '0' && i <= '9') {
+                result = result * 16 + (i - '0');
+            } else if (i >= 'A' && i <= 'F') {
+                result = result * 16 + (i - 'A' + 10);
+            } else if (i >= 'a' && i <= 'f') {
+                result = result * 16 + (i - 'a' + 10);
+            }
         }
-    }
 
-    return result;
+        return result;
+    } else if (isTwosComplement) {
+        return 0;
+    }
+    return 0;
 }
 
-std::string ALU::decToHex(int dec) {
-    std::string hexDigits = "0123456789ABCDEF";
-    std::string result;
+std::string ALU::decToHex(int dec, bool isTwosComplement, bool isFloatNotation) {
+    if (!isTwosComplement && !isFloatNotation) {
+        std::string hexDigits = "0123456789ABCDEF";
+        std::string result;
 
-    while (dec > 0) {
-        int remainder = dec % 16;
-        result.insert(0, 1, hexDigits[remainder]);
-        dec /= 16;
+        while (dec > 0) {
+            int remainder = dec % 16;
+            result.insert(0, 1, hexDigits[remainder]);
+            dec /= 16;
+        }
+        while (result.size() < 2) {
+            result.insert(0, 1, '0');
+        }
+
+        return result;
+    } else if (isTwosComplement) {
+        return {};
     }
-
-    while (result.size() < 2) {
-        result.insert(0, 1, '0');
-    }
-
-    return result;
+    return {};
 }
 
 //    Binary
-double ALU::binToDec(const std::string &bin) {
-    double result;
+int ALU::binToDec(const std::string &bin, bool isTwosComplement, bool isFloatNotation) {
+    if (!isTwosComplement && !isFloatNotation) {
+        int result = 0;
 
-    for (char i: bin) {
-        result = result * 2 + (i - '0');
+        for (char digit: bin) {
+            result = result * 2 + (digit - '0');
+        }
+
+        return result;
+    } else if (isTwosComplement) {
+        return 0;
     }
-    return result;
+    return 0;
 }
 
-std::string ALU::decToBin(int dec) {
-    std::string binDigits = "01";
-    std::string result;
+std::string ALU::decToBin(int dec, bool isTwosComplement, bool isFloatNotation) {
+    if (!isTwosComplement && !isFloatNotation) {
+        std::string binDigits = "01";
+        std::string result;
 
-    while (dec > 0) {
-        int remainder = dec % 2;
-        result.insert(0, 1, binDigits[remainder]);
-        dec /= 2;
+        while (dec > 0) {
+            int remainder = dec % 2;
+            result.insert(0, 1, binDigits[remainder]);
+            dec /= 2;
+        }
+        while (result.size() < 8) {
+            result.insert(0, 1, '0');
+        }
+
+        return result;
+    } else if (isTwosComplement) {
+        return {};
     }
-
-    while (result.size() < 8) {
-        result.insert(0, 1, '0');
-    }
-
-    return result;
+    return {};
 }
 
+//    Validation
 bool ALU::isValidInstruction(std::string instruction) {
     for (const auto &hex: instruction) {
         if (hex < '0' || hex > 'F') {
@@ -76,6 +96,4 @@ bool ALU::isValidInstruction(std::string instruction) {
 }
 
 //    Arithmetic
-int ALU::add(int number_1, int number_2) {
-    return 0;
-}
+
